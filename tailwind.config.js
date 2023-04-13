ee5f83a14f648ab173ed2212ab7a960cd5ffa4fe
TohidEq <TohidEq@gmail.com>
2023-04-13 20:44:38 +0330

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+    colors: {
+      'red-c': "#C04848",
+      'purple-c': "#480048",
+      'white-c': "#ffffff",
+      'black-c': "#0d0D0D"
+      
+
+
+    },
+  },
+  plugins: [],
+}
