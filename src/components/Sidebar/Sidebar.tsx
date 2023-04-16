8552e49dd212afb7a617970236cc47d9226de817
TohidEq <TohidEq@gmail.com>
2023-04-17 01:43:48 +0330

    simple design sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,62 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
+import {
+  faUserCircle as fasUser,
+  faHome as fasHome,
+  faHeart as fasHeart,
+  faSearch as fasSearch,
+  faMoon as fasMoon,
+  faQuestion as fasQues,
+} from "@fortawesome/free-solid-svg-icons";
+import { faUser as farUser } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
+
+import { Link, NavLink } from "react-router-dom";
+
+{
+  // <FontAwesomeIcon icon={farHeart} />
+  // <FontAwesomeIcon icon={faUser} />
+}
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
     <aside>
-      <div>T</div>
-      <div>T</div>
-      <FontAwesomeIcon icon={farHeart} />;
+      {/* Profile of SignIn/Up --> */}
+      <NavLink to={"/profile"} className="side-item side-profile">
+        <FontAwesomeIcon icon={fasUser} />
+        <span>Profile</span>
+      </NavLink>
+      {/* <-- end profile or SignIn/Up */}
+      <div className="side-links-container">
+        <div className="side-links">
+          <NavLink to={"/"} className="side-item" end>
+            <FontAwesomeIcon icon={fasHome} />
+            <span>Home</span>
+          </NavLink>
+
+          <NavLink to={"/search"} className="side-item">
+            <FontAwesomeIcon icon={fasSearch} />
+            <span>Search</span>
+          </NavLink>
+
+          <NavLink to={"/likes"} className="side-item">
+            <FontAwesomeIcon icon={fasHeart} />
+            <span>Likes</span>
+          </NavLink>
+        </div>
+
+        <div className="side-links ">
+          <button className="side-item">
+            <FontAwesomeIcon icon={fasMoon} />
+            <span>Theme</span>
+          </button>
+          <NavLink to={"/about-us"} className="side-item">
+            <FontAwesomeIcon icon={fasQues} />
+            <span>About Us</span>
+          </NavLink>
+        </div>
+      </div>
     </aside>
   );
 };
