16b711f28b7fd0b4ce45a9e48aaaa3ead68a1e88
TohidEq <TohidEq@gmail.com>
2023-05-15 21:45:05 +0330

    Create User page

---
+++ src/components/pages/profile/MyProfile.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../../actions/cartAction";
+
+type Props = {};
+
+const MyProfile = (props: Props) => {
+  const dispatch = useDispatch();
+  const signOut = () => {
+    sessionStorage.clear();
+    dispatch(logOut());
+  };
+  const myredux = useSelector(
+    (state: { id: string; isAuthenticated: boolean }) => state
+  );
+  return (
+    <div className="user">
+      <div className="user-basic-data">
+        <div className="user-img">user-img</div>
+        <div className="user-names">
+          <div className="user-real-name">Bla BlaZade</div>
+          <div className="username">@ blaBlabla</div>
+        </div>
+      </div>
+
+      <div className="user-summary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eos
+        animi! Quos, adipisci explicabo! Facilis rerum minus.
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,30 +1,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logIn, logOut } from "../../../actions/cartAction";
+import MyProfile from "./MyProfile";
 
 type Props = {};
 
 function Profile({}: Props) {
-  const dispatch = useDispatch();
-  const signOut = () => {
-    sessionStorage.clear();
-    dispatch(logOut());
-  };
-  const myredux = useSelector(
-    (state: { id: string; isAuthenticated: boolean }) => state
-  );
 
   return (
-    <div>
-      Profile
-      <button onClick={signOut}>(-logout--)</button>
-      <div
-        onClick={() => {
-          console.log("clicked");
-        }}
-      >
-        your redux:{myredux.id} and {myredux.isAuthenticated}
-      </div>
+    <div className="profile">
+      <MyProfile />
+      {/* Posts | Comments | Likes  */}
     </div>
   );
 }
